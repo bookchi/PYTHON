2d5566d0fe54fdf2157fadcc4fcf2c07df73c570
bookchi <1451342300@qq.com>
2018-05-17 11:13:35 +0800

    Update py_yield.py

--- py_yield.py
+++ py_yield.py
@@ -1,3 +1,6 @@
                                           yield key word
 Yield is a key word in python. It's a symbol of generator.
 A function with a yield is a generator.
+'''
+import requests
+'''
